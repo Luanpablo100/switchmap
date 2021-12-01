218e49478a89a30e72d9c037b9895dd230f8c706
Luan Oliveira <lpablo611@gmail.com>
2021-12-01 11:13:41 -0400

    Separate new Prisma Client from PrismaCommands

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -5,7 +5,7 @@ const ButtonElement = ({children, onClick, type}) => {
     return ( 
         <div className="buttons" onClick={onClick}>
             <div className="button-container">
-                <span className="btn effect01"><span><button type={type}>{children}</button></span></span>
+                <span className="btn effect01"><span>{children}</span></span>
             </div>
         </div>
      );
