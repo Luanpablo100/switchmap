5558ea1ec1f3dbfec095bb0f061d980d9d68f57b
Luan Oliveira <lpablo611@gmail.com>
2021-12-24 09:38:07 -0400

    ordenando portas

--- components/switchElement.js
+++ components/switchElement.js
@@ -8,11 +8,15 @@ export default function SwitchElement({sw, departments}) {
     let odd
     {odd = sw.Ports.filter(port => port.code % 2 !== 0)}
     {even = sw.Ports.filter(port => port.code % 2 === 0)}
+
+    const ordernedOdd = odd.sort((a,b) => a.code - b.code)
+    const ordernedEven = even.sort((a,b) => a.code - b.code)
+
     return (
         <div className={styles.switch}>
             <div>
-                {<Ports ports={odd} orientation={"up"} key={`${sw.id}-up`} departments={departments}/>}
-                {<Ports ports={even} orientation={"down"} key={`${sw.id}-down`} departments={departments}/>}
+                {<Ports ports={ordernedOdd} orientation={"up"} key={`${sw.id}-up`} departments={departments}/>}
+                {<Ports ports={ordernedEven} orientation={"down"} key={`${sw.id}-down`} departments={departments}/>}
             </div>
             <div className={styles.swcode}>
                 <h1>{sw.code}</h1>
